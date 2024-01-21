838f78be05961c56a2d34fbca14ab82e62dfc4db
fawwaz95 <fawwaz_95@hotmail.com>
2024-01-21 13:17:18 -0500

    able to fetch login and login with existing users, created logic to fetch register endpoint...still needs more work

--- src/components/Neutral/LoginPage.js
+++ src/components/Neutral/LoginPage.js
@@ -4,21 +4,21 @@ import Homepage from "../Neutral/HomePage";
 
 const LoginPage = () => {
     const navigate = useNavigate();
-    const emailOrUsernameRef = useRef();
+    const usernameRef = useRef();
     const passwordRef = useRef();
 
     useEffect(() => {
         console.log("LOGIN PAGE");
     })
 
-    const saveUserCredentials = async () => {
-        if (!emailOrUsernameRef.current.value || !passwordRef.current.value) {
+    const loginToApp = async () => {
+        if (!usernameRef.current.value || !passwordRef.current.value) {
             console.log("Please specify both email/username & password");
             return false;
         }
 
         const userCredentials = {
-            userName: emailOrUsernameRef.current.value,
+            userName: usernameRef.current.value,
             password: passwordRef.current.value,
         }
 
@@ -37,14 +37,14 @@ const LoginPage = () => {
             const response = await fetch("http://localhost:3001/routes/login", postMethod);
 
             if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
+                console.error(`HTTP error! status: ${response.status}`);
             }
 
             const data = await response.json();
 
             if(data.success === false){
                 console.log("Invalid user......");
-                console.log(data.message);
+                console.log(data.message); //Need to tell user, maybe show the username field in red?
             }else{
                 console.log("Login Successful");
                 console.log(data);
@@ -71,13 +71,13 @@ const LoginPage = () => {
                     <div className="text-3xl text-blue-400 py-8"> Login </div>
                 </div>
                 {/*<div className="font-inter text-3xl ml-3 mr-4 text-blue-400 pb-4"> Moodz </div>8*/}
-                <div className="self-start">Email</div>
-                <input type="text" className="w-full p-2 text-white bg-slate-800 rounded-md" ref={emailOrUsernameRef} />
+                <div className="self-start">Username</div>
+                <input type="text" className="w-full p-2 text-white bg-slate-800 rounded-md" ref={usernameRef} />
                 <div className="self-start pt-2">Password</div>
                 <input type="password" className="w-full p-2 mb-4 text-white bg-slate-800 rounded-md" ref={passwordRef} />
-                <button type="submit" className="w-full py-2 text-white bg-blue-400 rounded-md" onClick={e => {
+                <button type="submit" className="w-full py-2 text-white bg-blue-400 rounded-md" onClick={(e) => {
                     e.preventDefault()
-                    saveUserCredentials()
+                    loginToApp()
                 }}>Log in</button>
                 <div className="text-xs text-slate-400"> <a href="#"> Forgot your password? </a> </div>
             </div>
--- src/components/Neutral/RegisterPage.js
+++ src/components/Neutral/RegisterPage.js
@@ -1,32 +1,85 @@
+import { useRef } from "react";
 import { Link } from "react-router-dom";
 
 const RegisterPage = () => {
-    return(
-        <div className="h-screen flex flex-col items-center justify-center text-slate-400">
-            <div className="absolute top-20 right-0 p-4 text-white">
-                <Link to="/Login" className="flex items-center">
-                    <div className="mr-2">Login</div>
-                    <img src="/images/rightArrow_Icon.svg" className="h-8" alt="Right arrow icon" />
-                </Link>
-            </div>
-            <div className="flex flex-col items-center justify-center gap-2 w-96 p-2">
-                <div className="self-start">
-                    <div className="text-3xl text-blue-400 py-8"> Register </div>
-                </div>
-                <div className="self-start">First name</div>
-                <input type="text" className="w-full p-2 text-white bg-slate-800 rounded-md" />
-                <div className="self-start">Last name</div>
-                <input type="text" className="w-full p-2 text-white bg-slate-800 rounded-md" />
-                <div className="self-start">User name</div>
-                <input type="text" className="w-full p-2 text-blawhiteck bg-slate-800 rounded-md" />
-                <div className="self-start">Email</div>
-                <input type="text" className="w-full p-2 text-white bg-slate-800 rounded-md" />
-                <div className="self-start pt-2">Password</div>
-                <input type="password"  className="w-full p-2 mb-4 text-white bg-slate-800 rounded-md" />
-                <button type="submit" className="w-full py-2 text-white bg-blue-400 rounded-md">Register</button>
-            </div>
+    const fistNameRef = useRef();
+    const lastNameRef = useRef();
+    const userNameRef = useRef();
+    const emailRef = useRef();
+    const passwordRef = useRef();
+
+    const registerUser = async () => {
+        try {
+            const registerUser = {
+                firstName: fistNameRef.current.value,
+                lastName: lastNameRef.current.value,
+                userName: userNameRef.current.value,
+                email: emailRef.current.value,
+                password: passwordRef.current.value
+            }
+
+            const postMethod = {
+                method: "Post",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    firstName: registerUser.firstName,
+                    lastName: registerUser.lastName,
+                    userName: registerUser.userName,
+                    email: registerUser.email,
+                    password: registerUser.password,
+                }),
+            }
+
+            const response = await fetch("http://localhost:3001/routes/register", postMethod);
+
+            if (!response.ok) {
+                console.error("Http error: " + response.status);
+            }
+
+            const jsonData = await response.json();
+
+            if (jsonData && jsonData.success === true) {
+                console.log("User registered!!!");
+                console.log(jsonData);
+            }
+    }catch (error) {
+        console.error("Error registering user.......");
+        console.error(error.status + " " + error.message);
+    }
+}
+
+return (
+    <div className="h-screen flex flex-col items-center justify-center text-slate-400">
+        <div className="absolute top-20 right-0 p-4 text-white">
+            <Link to="/Login" className="flex items-center">
+                <div className="mr-2">Login</div>
+                <img src="/images/rightArrow_Icon.svg" className="h-8" alt="Right arrow icon" />
+            </Link>
         </div>
-    )
+        <div className="flex flex-col items-center justify-center gap-2 w-96 p-2">
+            <div className="self-start">
+                <div className="text-3xl text-blue-400 py-8"> Register </div>
+            </div>
+            <div className="self-start">First name</div>
+            <input type="text" className="w-full p-2 text-white bg-slate-800 rounded-md" ref={fistNameRef} />
+            <div className="self-start">Last name</div>
+            <input type="text" className="w-full p-2 text-white bg-slate-800 rounded-md" ref={lastNameRef} />
+            <div className="self-start">User name</div>
+            <input type="text" className="w-full p-2 text-blawhiteck bg-slate-800 rounded-md" ref={userNameRef} />
+            <div className="self-start">Email</div>
+            <input type="text" className="w-full p-2 text-white bg-slate-800 rounded-md" ref={emailRef} />
+            <div className="self-start pt-2">Password</div>
+            <input type="password" className="w-full p-2 mb-4 text-white bg-slate-800 rounded-md" ref={passwordRef} />
+            <button type="submit" className="w-full py-2 text-white bg-blue-400 rounded-md" onClick={(e) => {
+                e.preventDefault()
+                registerUser()
+            }}>
+                Register</button>
+        </div>
+    </div>
+)
 }
 
 export default RegisterPage;
