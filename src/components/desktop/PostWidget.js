3c4155147b31fdc0d7db9f12a7ffe4c520baf82d
fawwaz95 <fawwaz_95@hotmail.com>
2024-02-14 17:49:45 -0500

    added upload file func...still needs more work

--- src/components/desktop/PostWidget.js
+++ src/components/desktop/PostWidget.js
@@ -1,8 +1,55 @@
 
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { Link } from 'react-router-dom';
 
 const PostWidget = () => {
+    const fileInputRef = useRef(null);
+    const [uploadFile, setUploadFile] = useState({});
+
+    useEffect(() => {
+        console.log("Useeffecting.......Post widget");
+        console.log(uploadFile);
+
+        if(uploadFile && uploadFile.name){
+             fetchEndpoint(uploadFile);
+        }
+
+    }, [uploadFile])
+
+    const handleClick = () => {
+        fileInputRef.current.click();
+      };
+
+    const handleFileChange = async (event) => {
+        const file = event.target.files;
+        console.log('Selected files:', file[0].name);
+        setUploadFile(file[0]);
+    };
+
+    const fetchEndpoint = async (file) => {
+        console.log("What is file ? ");
+        console.log(file);
+
+        const formData = new FormData();
+        formData.append('file', file);
+
+        console.log("Form data");
+        console.log(formData);
+
+        const postMethod = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: formData,
+        };
+
+        const response = await fetch("http://localhost:3001/routes/fileUpload", postMethod);
+        const data = await response.json();
+
+        console.log("Heres the data fetchEndpoint " + data);
+    }
+
     return (
         <div id="postWidget" className="ml-8 text-sm text-white text-base rounded-md bg-zinc-900">
             <div className="flex p-4 border-b border-white">
@@ -15,7 +62,13 @@ const PostWidget = () => {
                     <div>Camera</div>
                 </div>
                 <div className="flex items-center">
-                    <a href="#"> <img src="/images/upload_icon.svg" className="h-8 pr-2" /> </a>
+                    <a href="#" onClick={handleClick}> <img src="/images/upload_icon.svg" className="h-8 pr-2" /> </a>
+                    <input
+                        type="file"
+                        ref={fileInputRef}
+                        style={{ display: 'none' }}
+                        onChange={handleFileChange}
+                    />
                     <div>Upload</div>
                 </div>
                 <button className="bg-sky-900 pl-8 pr-8 py-2 rounded-full"> Post </button>
